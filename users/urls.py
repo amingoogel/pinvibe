9b180e8dae0dbcdde51b7d5125eb280d03be66d8
Amin Chenaneh <amingoogel35@gmail.com>
2025-07-13 14:26:34 +0330

    Add temporary superuser creation view

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, create_superuser
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='pin_list'), name='logout'),
+    path('create-superuser/', create_superuser, name='create_superuser'),  # TEMPORARY - REMOVE AFTER USE
 ]
 
